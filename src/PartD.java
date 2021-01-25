7550f5409c987c53e5fe492c562dd3767885847e
iamjamesreuben <reubenjames1@myop.ac.nz>
2021-01-25 23:30:10 +1300

    This is the final commit.

--- src/PartD.java
+++ src/PartD.java
@@ -6,6 +6,7 @@ public class PartD {
 	System.out.println("My name is Reuben");
 	System.out.println("My favourite colour is black");
 	System.out.println("My favourite mode of transportation is car");
+	System.out.println("The best day of the week is Friday!");
 	
 	}
 }
